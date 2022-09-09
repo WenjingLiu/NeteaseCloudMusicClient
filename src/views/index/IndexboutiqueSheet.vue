1c57e72c893a28f26e7289780b52a09cc51f9528
lwj <wjliu@goodsmilearts.com>
2022-09-09 14:11:22 +0800

    ADD：精品歌单

--- src/views/index/IndexboutiqueSheet.vue
+++ src/views/index/IndexboutiqueSheet.vue
@@ -2,16 +2,28 @@
     <div class="boutique-song-sheet">
         <div class="boutique-song-sheet__title">
             <span>精品歌单</span>
-            <p class="boutique-song-sheet__title--active">{{ songsheetActive }}</p>
+            <song-sheet-tag-main v-model="songsheetActive" :list="tagList" direction="right"></song-sheet-tag-main>
         </div>
     </div>
 </template>
 <script setup>
 import { ref } from 'vue'
 import { useRoute }  from 'vue-router'
-
+import SongSheetTagMain from "./components/songSheet/SongSheetTagMain.vue";
+import {
+  getSongSheetBoutiqueTagApi,
+  getSongSheetBoutiqueApi,
+} from "@/api/index";
 const route = useRoute()
 const songsheetActive = ref(route.params.activeCategory)
+const tagList = ref({})
+getSongSheetBoutiqueTagApi().then((res) => {
+    console.log(res);
+    tagList.value = [{
+        name: '',
+        sub: res.data.tags
+    }]
+});
 
 </script>
 <style scoped lang="scss">
