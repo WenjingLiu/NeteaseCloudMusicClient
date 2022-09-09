8098bda926493fb9d0fbc9ede878b03c78144c5e
lwj <wjliu@goodsmilearts.com>
2022-09-09 14:12:11 +0800

    分页组件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="pagination-list">
+        <div class="pagination-item">></div>
+        <div class="pagination-item">1</div>
+        <div class="pagination-item">2</div>
+        <div class="pagination-item">3</div>
+        <div class="pagination-item">></div>
+    </div>
+</template>
+<script setup>
+defineProps({
+    currentPage: String,
+    pageSize: Number,
+    
+})
+</script>
+<style scoped lang="scss">
+.pagination {
+    &-list {
+        display: flex;
+        justify-items: center;
+        align-items: center;
+        max-width: 200px;
+        margin: 0 auto;
+    }
+    &-item {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        width: 30px;
+        height: 30px;
+        text-align: center;
+        line-height: 30px;
+    }
+    &-item + &-item {
+        margin-left: 5px;
+    }
+}
+</style>
